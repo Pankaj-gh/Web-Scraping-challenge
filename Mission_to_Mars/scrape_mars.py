8cfa45ee8a254d1a20be50e3371dc5b989c09994
Pankaj <tahiliani.pankaj19@gmail.com>
2020-10-26 18:06:31 -0700

    defining function scrape

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,5 +1,10 @@
+from flask import Flask, render_template
 from bs4 import BeautifulSoup
 import requests
 from splinter import Browser
 import pandas as pd
+
+
+app = Flask(__name__)
+
 def scrape():

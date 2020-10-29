932dd6f30cb09727e2329a156a84aa2c2abafa68
Pankaj <tahiliani.pankaj19@gmail.com>
2020-10-28 20:25:57 -0700

    placing hemishpheres in order

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -107,10 +107,10 @@ def scrape():
         img_urls.append(img_url)
     
 
-    hemisphere_image_urls=[{"title1":"Cerberus Hemisphere Enhanced","image_url1":"https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
-                        {"title2":"Schiaparelli Hemisphere Enhanced","image_url2":"https://astrogeology.usgs.gov/cache/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg"},
-                        {"title3":"Syrtis Major Hemisphere Enhanced","image_url3":"https://astrogeology.usgs.gov/cache/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg"},
-                        {"title4":"Valles Marineris Hemisphere Enhanced","image_url4":"https://astrogeology.usgs.gov/cache/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg"}]
+    hemisphere_image_urls=[{"title":"Cerberus Hemisphere Enhanced","image_url":"https://astrogeology.usgs.gov/cache/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg"},
+                        {"title":"Schiaparelli Hemisphere Enhanced","image_url":"https://astrogeology.usgs.gov/cache/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg"},
+                        {"title":"Syrtis Major Hemisphere Enhanced","image_url":"https://astrogeology.usgs.gov/cache/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg"},
+                        {"title":"Valles Marineris Hemisphere Enhanced","image_url":"https://astrogeology.usgs.gov/cache/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg"}]
                         
 
     
